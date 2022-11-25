5c481ef0302512045f175bdb7328eb47f263f85b
meher09 <mehernigarcu@gmail.com>
2022-11-25 22:08:15 +0600

    Added Login and Registration Page

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createContext } from "react";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from "../firebase/firebase.config";
 
-const AuthProvider = () => {
+
+export const AuthContext = createContext()
+
+const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider()
+
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const signInWithGoogle = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const authInfo = {
+        user,
+        setUser,
+        signInWithGoogle,
+        loading,
+        setLoading,
+    }
+
     return (
-        <div>
-            This is for Authentication
-        </div>
+        <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
     );
 };
 
